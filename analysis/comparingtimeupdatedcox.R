535d33491664ad77b50e25abe102a4743d8e05c6
Sarah Urbut <sarahurbut@Sarahs-MacBook-Pro.local>
2023-08-03 17:29:56 -0400

    Update

---
+++ analysis/comparingtimeupdatedcox.R
@@ -0,0 +1,5 @@
+# 2. Combined. This approach utilized an application of 10-year CVD risk calculators. For fairness of comparison, we estimated 10-year probabilities of survival based on our data. Three probabilities were calculated for each person: the first using baseline age, second using baseline age plus 10 years and third using baseline age plus 20 years, with all other risk factors based on the baseline values. The 30-year risk was calculated as the difference of 1 minus the product of these three 10-year probabilities;
+# 
+# 3. Unadjusted. Here we applied the standard Cox model to our data with full follow-up ignoring competing risk of death;
+# 4. Adjusted. This is our main approach following the model described earlier above. The above four methods were applied to individuals with different combinations of risk factors for hard CVD events.
+# The authors had full access to the data and take responsibility for its integrity. All authors have read and agree to the manuscript as written.
