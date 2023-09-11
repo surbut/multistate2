7124bde705dc7e38c842d8135b1b8a7a10418faa
Sarah Urbut <sarahurbut@Sarahs-MacBook-Pro.local>
2023-09-11 13:03:41 -0500

    update

--- Untitled.R
+++ Untitled.R
@@ -1,31 +1,46 @@
 library(visNetwork)
 
-# Nodes data with reduced label text
+# Nodes data
 nodes <- data.frame(
   id = 1:10, 
-  label = c("Health", "Diabetes", "Hypertension", "HyperLipid", 
-            "D + Hypertension", "Hypertension + HyperLipid", "HyperLipid + D", 
-            "Triple", "CAD", "Death"),
+  label = c("Health", 
+            "Diabetes", "Hypertension", "Hyperlipidemia", 
+            "Diabetes + Hypertension", "Hypertension + Hyperlipidemia", "Hyperlipidemia + Diabetes", 
+            "Triple risk", "CAD", "Death"),
   level = c(1, 2, 2, 2, 3, 3, 3, 4, 5, 5),
   color = c("lightblue", "lightcoral", "lightcoral", "lightcoral", 
             "lightgreen", "lightgreen", "lightgreen", "lightyellow", "grey", "black"),
-  font.size = 25,  # adjusted font size
-  size = 25  # adjusted node size
+  font.size = 16,
+  size = 25
 )
 
 # Edges data
 edges <- data.frame(
-  from = c(1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 9),
-  to = c(2, 3, 4, 9, 10, 5, 9, 10, 6, 9, 10, 7, 9, 10, 8, 9, 10, 8, 9, 10, 8, 9, 10, 9, 10, 10)
+  from = c(1, 1, 1, 1, 1, 
+           2, 2, 2, 2, 
+           3, 3, 3, 3,
+           4, 4, 4, 4,
+           5, 5, 5, 
+           6, 6, 6, 
+           7, 7, 7, 
+           8, 8,
+           9),
+  to = c(2, 3, 4, 9, 10, 
+         5, 7, 9, 10,
+         5, 6, 9, 10,
+         6, 7, 9, 10,
+         8, 9, 10, 
+         8, 9, 10,
+         8, 9, 10,
+         9, 10,
+         10)
 )
 
 # Plot
-visNetwork(nodes, edges, width = "100%", height = "1000px") %>%
+visNetwork(nodes, edges, width = "100%", height = "600px") %>%
   visLayout(hierarchical = list(enabled = TRUE, direction = "UD", levelSeparation = 300)) %>%
   visEdges(arrows = "to") %>%
   visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE) %>%
   visPhysics(
     hierarchicalRepulsion = list(centralGravity = 0.0, springLength = 300, springConstant = 0.05, nodeDistance = 350)
   )
-
-
