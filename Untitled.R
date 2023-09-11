6a712954595894ac2693c44fb379a04eb5bc5444
Sarah Urbut <sarahurbut@Sarahs-MacBook-Pro.local>
2023-09-11 12:53:06 -0500

    update

--- Untitled.R
+++ Untitled.R
@@ -1,20 +1,31 @@
-# Nodes data with adjusted levels and colors
+library(visNetwork)
+
+# Nodes data with reduced label text
 nodes <- data.frame(
-  id = 1:8, 
-  label = c("Health", "Diabetes", "Hypertension", "Hyperlipidemia", "Double risk factor", 
-            "Triple risk factor", "CAD", "Death"),
-  level = c(1, 2, 2, 2, 3, 3, 4, 4),
-  color = c("lightblue", "lightcoral", "lightcoral", "lightcoral", "lightgreen", "lightyellow", "grey", "black")
+  id = 1:10, 
+  label = c("Health", "Diabetes", "Hypertension", "HyperLipid", 
+            "D + Hypertension", "Hypertension + HyperLipid", "HyperLipid + D", 
+            "Triple", "CAD", "Death"),
+  level = c(1, 2, 2, 2, 3, 3, 3, 4, 5, 5),
+  color = c("lightblue", "lightcoral", "lightcoral", "lightcoral", 
+            "lightgreen", "lightgreen", "lightgreen", "lightyellow", "grey", "black"),
+  font.size = 25,  # adjusted font size
+  size = 25  # adjusted node size
 )
 
 # Edges data
 edges <- data.frame(
-  from = c(1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 7),
-  to = c(2, 3, 4, 7, 8, 5, 7, 8, 5, 7, 8, 5, 7, 8, 6, 7, 8, 7, 8, 8)
+  from = c(1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 9),
+  to = c(2, 3, 4, 9, 10, 5, 9, 10, 6, 9, 10, 7, 9, 10, 8, 9, 10, 8, 9, 10, 8, 9, 10, 9, 10, 10)
 )
 
 # Plot
-visNetwork(nodes, edges, width = "100%") %>%
-  visLayout(hierarchical = list(enabled = TRUE, direction = "UD", levelSeparation = 150)) %>%
+visNetwork(nodes, edges, width = "100%", height = "1000px") %>%
+  visLayout(hierarchical = list(enabled = TRUE, direction = "UD", levelSeparation = 300)) %>%
   visEdges(arrows = "to") %>%
-  visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE)
+  visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE) %>%
+  visPhysics(
+    hierarchicalRepulsion = list(centralGravity = 0.0, springLength = 300, springConstant = 0.05, nodeDistance = 350)
+  )
+
+
